b0e8a11cd70eaf6c0122512fd83ef93de471f46f
MrGuss <bednyakov04@mail.ru>
2023-04-08 18:18:54 +0300

    Added filter

--- main/forms.py
+++ main/forms.py
@@ -5,8 +5,8 @@ class BuildingForm(forms.ModelForm):
     objState = forms.CharField(label = 'Округ', max_length=6, required=True) #Here we can use ChoiceField
     objDistrict = forms.CharField(label = 'Район', max_length=30, required=True)
     objAddress = forms.CharField(label = 'Адрес', max_length=120, required=True)
-    objType = forms.CharField(label = 'Тип объекта', max_length=16, required=True)
-    objStatus = forms.CharField(label = 'Состояние объекта', max_length=16, required=True)
+    objType = forms.CharField(label = 'Тип объекта', max_length=20, required=True)
+    objStatus = forms.CharField(label = 'Состояние объекта', max_length=20, required=True)
     objArea = forms.IntegerField(label = 'Площадь объекта', required=True)
     objOwner = forms.CharField(label = 'Собственник', max_length=50, required=True)
     objUser = forms.CharField(label = 'Фактический пользователь', max_length=50, required=True)
--- main/views.py
+++ main/views.py
@@ -1,19 +1,48 @@
 from django.shortcuts import render, redirect
 from .forms import BuildingForm
+from .models import Building
 
-def test(request):
-    return render(request, 'main/index.html')
+#def test(request):
+#    return render(request, 'main/index.html')
 
 def createBuilding(request):
-    print("createBuilding method called")
     if request.method == 'POST':
         form = BuildingForm(request.POST, request.FILES)
         if form.is_valid():
-            print("form is valid")
             form.save()
         else:
-            print(request.FILES)
             print(form.errors)
     else:
         form = BuildingForm()
     return render(request, 'createBuilding.html', {'form': form})
+
+def filterBuilding(request):
+    queryset = Building.objects.all()
+    print(Building.objects.all())
+    
+    if request.method == 'POST':
+        min_area = request.POST.get('min_area')
+        max_area = request.POST.get('max_area')
+        state = request.POST.get('state')
+        tp = request.POST.get('type')
+        status = request.POST.get('status')
+        print(queryset[0].objImage)
+
+        if min_area and max_area:
+            queryset = queryset.filter(area__gte=min_area, area__lte=max_area)
+        elif min_area:
+            queryset = queryset.filter(area__gte=min_area)
+        elif max_area:
+            queryset = queryset.filter(area__lte=max_area)
+        
+        if state:
+            queryset = queryset.filter(objState=state)
+        if tp:
+            queryset = queryset.filter(objType=tp)
+        if status:
+            queryset = queryset.filter(objStatus=status)
+    context = {
+        'buildings': queryset,
+    }
+    print(context)
+    return render(request, 'main/index.html', context)
