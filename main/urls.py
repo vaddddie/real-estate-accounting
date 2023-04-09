c4c1b4f0c1eb8d68707ddc1437839044baf8b062
MrGuss <bednyakov04@mail.ru>
2023-04-09 09:41:14 +0300

    Fixed some problems

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.filterBuilding),
-    path('/createBuilding/', views.createBuilding, name='createBuilding'),
+    path('createBuilding/', views.createBuilding, name='createBuilding'),
     path('editBuilding/', views.edit_building, name='editBuilding'),
     path('editBuilding/<int:pk>/', views.BuildingUpdateView.as_view(), name='edit_building')
     
--- main/views.py
+++ main/views.py
@@ -3,19 +3,17 @@ from .forms import BuildingForm
 from .models import Building
 from django.views.generic.edit import UpdateView
 
-#def test(request):
-#    return render(request, 'main/index.html')
-
 def createBuilding(request):
     if request.method == 'POST':
         form = BuildingForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-        else:
+            return redirect("/")
+        else: 
             print(form.errors)
     else:
         form = BuildingForm()
-    return render(request, 'createBuilding.html', {'form': form})
+    return render(request, 'main/createBuilding.html', {'form': form})
 
 def filterBuilding(request):
     queryset = Building.objects.all()
@@ -36,11 +34,11 @@ def filterBuilding(request):
         elif max_area:
             queryset = queryset.filter(area__lte=max_area)
         
-        if state:
+        if (state!="Любой"):
             queryset = queryset.filter(objState=state)
-        if tp:
+        if (tp!="Любое"):
             queryset = queryset.filter(objType=tp)
-        if status:
+        if (status!="Любое"):
             queryset = queryset.filter(objStatus=status)
     context = {
         'buildings': queryset,
@@ -52,10 +50,11 @@ def edit_building(request):
     if request.method == 'POST':
         building_id = request.POST.get('building_id')
         return redirect("/editBuilding/"+str(building_id))
-    return render(request, 'editBuilding.html')
+    return render(request, 'main/editBuilding.html')
     
 class BuildingUpdateView(UpdateView):
     model = Building
-    fields = ['objState', 'objDistrict', 'objAddress', 'objType', 'objStatus', 'objArea', 'objOwner', 'objUser', 'objImage']
+    form_class = BuildingForm
+    #fields = ['objState', 'objDistrict', 'objAddress', 'objType', 'objStatus', 'objArea', 'objOwner', 'objUser', 'objImage']
     template_name_suffix = '_update_form'
     success_url= '/'
