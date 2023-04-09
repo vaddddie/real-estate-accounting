10a75d08a2ab0c3c9074209d3c0f14b972d1abf5
MrGuss <bednyakov04@mail.ru>
2023-04-09 11:57:46 +0300

    Fixed chosing problems

--- main/views.py
+++ main/views.py
@@ -17,16 +17,14 @@ def createBuilding(request):
     return render(request, 'main/createBuilding.html', {'form': form})
 
 def filterBuilding(request):
-    queryset = Building.objects.all()
-    print(Building.objects.all())
-    
+    queryset = Building.objects.all()    
     if request.method == 'POST':
         min_area = request.POST.get('min_area')
         max_area = request.POST.get('max_area')
         state = request.POST.get('state')
         tp = request.POST.get('type')
         status = request.POST.get('status')
-        print(queryset[0].objImage)
+        print(state)
 
         if min_area and max_area:
             queryset = queryset.filter(objArea__gte=min_area, objArea__lte=max_area)
@@ -35,16 +33,15 @@ def filterBuilding(request):
         elif max_area:
             queryset = queryset.filter(objArea__lte=max_area)
         
-        if (state!="Любой"):
+        if (state!="Любой") & (state!=None) :
             queryset = queryset.filter(objState=state)
-        if (tp!="Любое"):
+        if (tp!="Любое") & (tp!=None):
             queryset = queryset.filter(objType=tp)
-        if (status!="Любое"):
+        if (status!="Любое") & (status!=None):
             queryset = queryset.filter(objStatus=status)
     context = {
         'buildings': queryset,
     }
-    print(context)
     return render(request, 'main/index.html', context)
 
 def edit_building(request):
